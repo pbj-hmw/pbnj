2fefc6824db2154ea3a1000fdcac64722a3b4afa
Chris Piggott <chris@homebase.ai>
2018-07-21 21:49:37 -0500

    Home Cooked

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    PBNJ
+                    HomeCooked
                 </div>
             </div>
         </div>
