27fadb1428d0c92b1403122a232fdfcd44d70efb
Alex Todd <atodd@atodd.io>
2018-07-21 21:52:28 -0500

    Create/Delete Chat Channels

--- app/Services/ShowService.php
+++ app/Services/ShowService.php
@@ -1,26 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chris
- * Date: 7/21/18
- * Time: 7:17 PM
- */
 
 namespace App\Services;
 
-
 use App\Factory\ResultFactory;
-
 use App\Models\Show;
+use App\Services\TwilioService;
 
+/**
+ * Class ShowService
+ * @package App\Services
+ */
 class ShowService
 {
+    /**
+     * @var ResultFactory
+     */
     protected $resultFactory;
 
-    public function __construct(ResultFactory $resultFactory) {
-        $this->resultFactory = $resultFactory;
-    }
+    /**
+     * @var TwilioService
+     */
+    protected $twilioService;
 
+    /**
+     * ShowService constructor.
+     *
+     * @param ResultFactory $resultFactory
+     * @param TwilioService $twilioService
+     */
+    public function __construct(ResultFactory $resultFactory, TwilioService $twilioService)
+    {
+        $this->resultFactory = $resultFactory;
+        $this->twilioService = $twilioService;
+    }
 
     /**
      *
@@ -29,15 +41,21 @@ class ShowService
      * @param $properties array Values are default of what model expects. Check controller validation for exact.
      * @return object
      */
-    public function createShow($properties) {
+    public function createShow($properties)
+    {
         try {
             $show = Show::create($properties);
+
+            $chat_created = $this->twilioService->createChannel($show->title);
+            if (!$chat_created->success) {
+                return $chat_created;
+            }
+            $show->chat_sid = $chat_created->channel->sid;
+            $show->save();
 
             return $this->resultFactory->success($show, 'show');
         } catch (\Exception $e) {
             return $this->resultFactory->error("Unable to create new Show");
         }
-
-        //TODO: Create Twilio chat to associate to a chat.
     }
 }
--- config/pbnj.php
+++ config/pbnj.php
@@ -4,4 +4,7 @@ return [
     'twilio_account_sid' => env('TWILIO_ACCOUNT_SID'),
     'twilio_auth_token' => env('TWILIO_AUTH_TOKEN'),
     'twilio_number' => env('TWILIO_NUMBER'),
+    'twilio_api_key' => env('TWILIO_API_KEY'),
+    'twilio_api_secret' => env('TWILIO_API_SECRET'),
+    'twilio_service_sid' => env('TWILIO_SERVICE_SID')
 ];
